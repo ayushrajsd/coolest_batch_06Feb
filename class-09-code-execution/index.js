c5e3f8be9cfe89c5b582bba2db49e324a357b07c
Ayush <ayush.raj.sd@gmail.com>
2024-03-01 11:31:33 +0530

    lecture 9 execution context

---
+++ class-09-code-execution/index.js
@@ -0,0 +1,47 @@
+// // var a = 2
+// // var b = 3
+
+// // function add(num1, num2) {
+// //   var ans = num1 + num2
+// //     return ans
+// // }
+
+// // let add1 = add(a, b)
+// // let add2 = add(5,6)
+
+// // console.log(add1) // 5
+// // console.log(add2) // 11
+
+
+// var n = 3
+// function square(num){
+//   var ans = num * num
+//   return ans
+// }
+
+// const square1 = square(n)
+// // console.log(square1)
+
+// var someVal = "Hello World"
+// // console.log(msg)
+// var msg = "hello"
+
+// function greet(){
+//   var val = "hi"
+//   console.log(msg,val,"happy new year")
+// }
+// console.log(msg)
+// greet()
+
+console.log(a)
+printName()
+let a = 4
+
+function printName(){
+  console.log("My name is Mr. X")
+}
+
+let printAge = function(){
+  console.log("My age is 25")
+}
+printAge()
